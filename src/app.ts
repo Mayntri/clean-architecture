675c5a4b9264c13721b3a561c8b1c62f93f14423
Jort Kuhlmann <jortkuhlmann1@gmail.com>
2022-06-20 00:01:52 +0200

    fix

--- src/app.ts
+++ src/app.ts
@@ -21,7 +21,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(compression());
-app.use(expressWinston.logger(logger));
+// app.use(expressWinston.logger(logger));
 
 app.use(routes);
 

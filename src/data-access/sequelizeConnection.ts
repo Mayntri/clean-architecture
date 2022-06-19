93a5e6ba8c00421115a94058d503153d326fa630
Jort Kuhlmann <jortkuhlmann1@gmail.com>
2022-06-20 00:00:36 +0200

    fix

--- src/data-access/sequelizeConnection.ts
+++ src/data-access/sequelizeConnection.ts
@@ -4,7 +4,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 const sequelizeConnection = new Sequelize(process.env.DATABASE_URL || '', {
-  logging: !process.env.DB_LOGGING ? console.log : false,
+  logging: false,
 });
 
 export default sequelizeConnection;
